b92e4848dd91cc4ba14068cd3ecaba042f0872c0
priyan2115 <priyankaa2115@gmail.com>
2023-02-02 00:44:01 -0500

    challange assignment deliverable 3 coding complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -21,3 +21,17 @@ lot_summary<-S_coil_df_table%>% group_by(Manufacturing_Lot)%>%summarize(PSI_Mean
                                                                         PSI_Median=median(PSI),
                                                                         PSI_Var=var(PSI),
                                                                         PSI_Std_Dev=sd(PSI),.groups = 'keep')
+
+#using the t.test() function to determine if the PSI across all manufacturing lots 
+#is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(S_coil_df_table$PSI,mu=1500)
+
+#using the t.test() function and its subset() argument to determine if the PSI for each
+#manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+lot1<-subset(S_coil_df_table,Manufacturing_Lot=="Lot1")
+lot2<-subset(S_coil_df_table,Manufacturing_Lot=="Lot2")
+lot3<-subset(S_coil_df_table,Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
