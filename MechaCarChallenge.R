e3ee7721fff1fcdb5859595ad47a79a5a2ccdff7
priyan2115 <priyankaa2115@gmail.com>
2023-02-02 00:27:13 -0500

    challange assignment deliverable 2 coding complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,18 @@ mpg_df_table<- read.csv(file='MechaCar_mpg.csv', check.names = F, stringsAsFacto
 lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mpg_df_table)
 #Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
 summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mpg_df_table))
+
+#import and read suspension_coil.csv
+library(tidyverse)
+S_coil_df_table<- read.csv(file='Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+
+#create total_summary() and summarize all calculation like mean,median,sd on PSI col
+total_summary<- S_coil_df_table %>% summarize(PSI_Mean=mean(PSI),
+                                             PSI_Median=median(PSI),
+                                             PSI_Var=var(PSI),
+                                             PSI_Std_Dev=sd(PSI),.groups = 'keep')
+#craete lot_summary() and used group_by() and summarize() to group all calculations
+lot_summary<-S_coil_df_table%>% group_by(Manufacturing_Lot)%>%summarize(PSI_Mean=mean(PSI),
+                                                                        PSI_Median=median(PSI),
+                                                                        PSI_Var=var(PSI),
+                                                                        PSI_Std_Dev=sd(PSI),.groups = 'keep')
